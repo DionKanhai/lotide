220d407de436ae89504b3470055441f467ff81e8
DionKanhai <dionkanhais@gmail.com>
2022-08-13 14:29:23 +0000

    AssertEqual debug function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed");
+  } else if (actual !== expected) {
+    console.log("Assertion Failed");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
