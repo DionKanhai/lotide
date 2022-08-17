6285af9202b06060e2cd89308289050d8086b5c7
DionKanhai <dionkanhais@gmail.com>
2022-08-17 21:45:10 +0000

    added an if statement to get rid of spaces in input stringsToCount

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed`);
   } else if (actual !== expected) {
@@ -8,20 +8,22 @@ const assertEqual = function(actual, expected) {
 
 // stringsToCount is any amount of strings  
 // countLetters will count the frequency of each letter in the stringsToCount
-const countLetters = function(stringsToCount) {
+const countLetters = function (stringsToCount) {
   const results = {};
-  for (const strings of stringsToCount) {
-    if (results[strings]) {
-      results[strings] += 1; // if key exists in results object increment value by 1
-    } else {
-    results[strings] = 1;  // if key does not exist in results object then create the key in the results object with a value of 1
+  for (const character of stringsToCount.toLowerCase()) {
+    if (character !== ' ') {
+      if (results[character]) {
+        results[character] += 1; // if key exists in results object increment value by 1
+      } else {
+        results[character] = 1;  // if key does not exist in results object then create the key in the results object with a value of 1
+      }
     }
   }
   return results;
 }
 
 // test code 
-console.log(countLetters("Lighthouse Labs"))
+console.log(countLetters("Lighthouse labs"))
 
 // step 1. break the string into individual letters
 // step 2. loop through the string input of stringsToCount
