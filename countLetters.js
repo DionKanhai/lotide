dc81a47ca10fdf9f1f08c975bbaef433c80b2e48
DionKanhai <dionkanhais@gmail.com>
2022-08-17 17:11:42 +0000

    added a function that takes a string as an input and then returns an object with the individual strings as keys and the frequency of the string as values

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed`);
+  }
+};
+
+// stringsToCount is any amount of strings  
+// countLetters will count the frequency of each letter in the stringsToCount
+const countLetters = function(stringsToCount) {
+  const results = {};
+  for (const strings of stringsToCount) {
+    if (results[strings]) {
+      results[strings] += 1; // if key exists in results object increment value by 1
+    } else {
+    results[strings] = 1;  // if key does not exist in results object then create the key in the results object with a value of 1
+    }
+  }
+  return results;
+}
+
+// test code 
+console.log(countLetters("Lighthouse Labs"))
+
+// step 1. break the string into individual letters
+// step 2. loop through the string input of stringsToCount
+// step 3. each string has an index value 
+// step 4. if two index values match add them 
