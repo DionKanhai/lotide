263d38d6db9d91e1103d1408c8b9437955ded561
DionKanhai <dionkanhais@gmail.com>
2022-08-18 02:55:12 +0000

    refactored function properties

--- eqArrays.js
+++ eqArrays.js
@@ -7,10 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 function eqArrays(array1, array2) {
-  if (array1.join() === array2.join()) {
-    return true;
-  } else if (array1.join() !== array2.join()) {
+  console.log(array2)
+  if (array1.length !== array2.length) {
     return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
