0e07cec959c87d5585dc6b32201d89ea5159bce8
DionKanhai <dionkanhais@gmail.com>
2022-08-18 03:09:46 +0000

    updated eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,14 @@
 function eqArrays(array1, array2) {
-  if (array1.join() === array2.join()) {
-    return true;
-  } else if (array1.join() !== array2.join()) {
+  console.log(array2)
+  if (array1.length !== array2.length) {
     return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const assertArraysEqual = function(array1, array2) {
