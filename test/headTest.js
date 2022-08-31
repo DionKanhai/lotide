cd5e4624a4338c27e6964099032e7cdafdf993b0
DionKanhai <dionkanhais@gmail.com>
2022-08-31 15:27:50 +0000

    changed the testing method to mocha and chai testing method

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,23 @@
-// import the functions from their source files
-const assertEqual = require('../assertEqual')
+// // import the functions from their source files
+const assert = require('chai').assert;
 const head = require('../head')
 
-// Test code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 4);
+// Test code for head function 
+// using Mocha functions and Chai assertion function library
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
