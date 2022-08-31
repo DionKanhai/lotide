89505c605930dbd10b2a958311888f4642f04634
DionKanhai <dionkanhais@gmail.com>
2022-08-31 05:26:39 +0000

    refactored the file to separate the function definition and the test file in separate files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,6 @@
-function eqArrays(array1, array2) {
-  console.log(array2)
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
+// Function that asserts if two arrays match or not
 const assertArraysEqual = function(array1, array2) {
+  // check if arrays match
   if (array1 === array2) {
     console.log(`The Arrays match!`);
   } else if (array1 !== array2) {
@@ -19,4 +8,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+// Enable this function to be exported to other files
+module.exports = assertArraysEqual;
