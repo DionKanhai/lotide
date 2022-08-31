3a27e268bea82192c87f84b0e0f3dcb63f285c37
DionKanhai <dionkanhais@gmail.com>
2022-08-31 01:33:49 +0000

    added a separate file for the test code for the assertEqual function

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+// imports the function from the source file
+const assertEqual = require('../assertEqual');
+
+//Test code for the assertEqual function
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse", "Lighthouse");
