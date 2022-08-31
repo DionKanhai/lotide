7dc3afdea1ee4ee83a6a8f37af2f6b1917cd0a7a
DionKanhai <dionkanhais@gmail.com>
2022-08-31 14:27:53 +0000

    refactored the file to separate the function definition and the test file in separate files

--- middle.js
+++ middle.js
@@ -1,17 +1,19 @@
+// Function that finds the middle number of an array depending on length
 const middle = function(arrays) {
   let arrayLength = arrays.length
   let newArray = [];
+  // returns empty array for arrays with 2 or less numbers
   if (arrayLength < 3) {
     return [];
-  }
+  };
+  // finds index of middle number when length of array is odd
   if (arrayLength % 2 !== 0) {
-    newArray.push(arrays[Math.floor(arrayLength / 2)])
-    return newArray;
+    newArray.push(arrays[Math.floor(arrayLength / 2)]);
+    // finds index of middle numbers when length of array is even
   } if (arrayLength % 2 === 0) {
-    newArray.push(arrays[(arrayLength / 2)], arrays[(arrayLength / 2 ) - 1])
-  return newArray;
+    newArray.push(arrays[(arrayLength / 2)], arrays[(arrayLength / 2 ) - 1]);
   } 
-}
-// test code
-console.log(middle([1, 2, 4, 4]));
+  return newArray;
+};
 
+module.exports = middle;
