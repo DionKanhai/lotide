2df0b060bfe991f110f1cc64e9697efbfa09f0e0
DionKanhai <dionkanhais@gmail.com>
2022-09-03 23:07:51 +0000

    refactored the file and added comments for better readability

--- without.js
+++ without.js
@@ -1,28 +1,25 @@
-function eqArrays(array1, array2) {
-  if (array1.join() === array2.join()) {
-    return true;
-  } else if (array1.join() !== array2.join()) {
-    return false;
-  }
-}
+// Imports files for testing purposes
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(array1, array2) {
-  if (array1 === array2) {
-    console.log(`The Arrays match!`);
-  } else if (array1 !== array2) {
-    console.log(`The Arrays do not match!`);
-  }
-};
-
+// This function will receive two arrays
+// The second array will determine aa value to remove from the first array
+// The function will remove that value and return a new array without the value
 function without(source, itemsToRemove) {
+  // loop through the source array
   for (let i = 0; i < itemsToRemove.length; i++) {
+    // store the value to be removed in a variable
     const remove = source.includes(itemsToRemove[i])
+    // if vale is found in array splice it and return new array
     if (remove) {
       source.splice(i, 1)
     }
   }
   return source;
-}
-console.log(without([1, 2, 3], [1]))
+};
 
+// Test code
+console.log(without([1, 2, 3], [1]));
+console.log(without([4, 5], [4]));
+
+assertArraysEqual(without([1, 2, 3], [1]));
 
