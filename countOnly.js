f5a0fb0626bcdff6cb781fb661fb34645ffc425e
DionKanhai <dionkanhais@gmail.com>
2022-09-03 23:33:19 +0000

    refactored test code and added comments to countOnly function for better readability

--- countOnly.js
+++ countOnly.js
@@ -1,27 +1,27 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed`);
-  }
-};
+// Imports the files needed to test the function
+const assertEqual = require('./assertEqual');
 
+// This function is passed two parameters:
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+// It will return an object with the name of the key and count found in allItems (input object)
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
-  for (const item of allItems) {
-    if (itemsToCount[item]) { 
+  // loop through input object
+  for (const item of allItems) { 
+    // if the item in input array matches with the itemsToCount array increment by 1 in results object
+    if (itemsToCount[item]) {   
       if (results[item]) {
         results[item] += 1;
-      } else {
+      } 
+       // if the item in input array matches with the itemsToCount array add it to results object and add 1
+      else {
         results[item] = 1;
       }
     }
   }
   return results;
-}
+};
 
 // test code
 const firstNames = [
@@ -42,3 +42,5 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
