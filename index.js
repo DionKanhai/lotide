4efc94c6f5368a6a3f503c26757f94aeb2e46683
DionKanhai <dionkanhais@gmail.com>
2022-09-03 18:31:27 +0000

    refactored the index file to use property value shorthand syntax

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Project Lotide index of all the utility functions
 
 // Import all the utility files from the Lotide directory using require method
-const head = require('./head'); // returns a new array with the first element from an input array 
-const tail = require('./tail'); // returns a new array that includes all elements but the first element from an input array 
-const middle = require('./middle'); // returns a new array with the middle elements from an input array
+const head = require('./head'); 
+const tail = require('./tail'); 
+const middle = require('./middle'); 
 const assertArraysEqual = require('./assertArraysEqual'); 
 const assertEqual = require('./assertEqual');
 const countLetters = require('./countLetters');
@@ -21,20 +21,20 @@ const without = require('./without');
 // Exporting all utility functions as an object
 // Object that contains all of the utility functions as key value pairs
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue, 
-  flatten: flatten,
-  letterPositions: letterPositions, 
-  map: map,
-  takeUntil: takeUntil, 
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue, 
+  flatten,
+  letterPositions, 
+  map,
+  takeUntil,
+  without
 };
