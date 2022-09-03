2b218a537fd73a9f689b04d7f973438133e20fd2
DionKanhai <dionkanhais@gmail.com>
2022-09-03 19:09:51 +0000

    added comments to make file more understandable

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,9 @@
-// Function that asserts if two arrays match or not
+// Function that is passed in an two arrays and outputs an assertion message if the two arrays match or not
 const assertArraysEqual = function(array1, array2) {
-  // check if arrays match
+  // check if arrays match and if they do logs the message
   if (array1 === array2) {
     console.log(`The Arrays match!`);
+    // checks if two do not match and logs the message
   } else if (array1 !== array2) {
     console.log(`The Arrays do not match!`);
   }
