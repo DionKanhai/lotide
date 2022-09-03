ad87d0bd0614a3821766762a96f2777b8a1c92d6
DionKanhai <dionkanhais@gmail.com>
2022-09-03 23:20:51 +0000

    refactored the code and added comments for better readability

--- letterPositions.js
+++ letterPositions.js
@@ -1,18 +1,5 @@
-function eqArrays(array1, array2) {
-  if (array1.join() === array2.join()) {
-    return true;
-  } else if (array1.join() !== array2.join()) {
-    return false;
-  }
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (array1 === array2) {
-    console.log(`The Arrays match!`);
-  } else if (array1 !== array2) {
-    console.log(`The Arrays do not match!`);
-  }
-};
+// Imports the files needed to test the function
+const assertArraysEqual = require('./assertArraysEqual');
 
 /* function that will take a sentence and return an object with the key (individual strings) 
 and value (all indices where the string is found)*/
@@ -20,17 +7,21 @@ const letterPositions = function(sentence) {
   const results = {};
   console.log(results)
   for (let i = 0; i < sentence.length; i++) {
+    //variable that stores the individual letter positions of the input string in the loop
     const character = sentence[i];
+    // skip spaces
     if (character !== ' ') {
       if (!results[character]) {
         results[character] = [];
       }
+      // push the individual letter positions of the input string to the results object
       results[character].push(i)
     }
   }
   return results;
-}
+};
 
 //test code
 console.log(letterPositions(`hello world`));
+assertArraysEqual(letterPositions("hello").e, [1]);
 
