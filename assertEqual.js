a8741204ac14d938727f1ff69557d7a31e080484
DionKanhai <dionkanhais@gmail.com>
2022-09-03 19:10:32 +0000

    added comments to make file more understandable

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,10 @@
-// function that asserts if two values are equal or not
+// function that is passed a value and checks if it matches the second value passed in
+// and then asserts if two values are equal or not
 const assertEqual = function(actual, expected) {
+// if the input matches the output then assert passed  
   if (actual === expected) {
     console.log(`Assertion Passed`);
+// if the input does not match the output then assert fail
   } else if (actual !== expected) {
     console.log(`Assertion Failed`);
   }
