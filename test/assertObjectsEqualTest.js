b00a5ed9076e11828b0c42e49a5437dd944487e9
DionKanhai <dionkanhais@gmail.com>
2022-09-03 19:53:34 +0000

    added more comments for better readability

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -2,11 +2,13 @@
 const assertObjectsEqual = require('../assertObjectsEqual');
 
 
-// test code 
+// Test code 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
+// Passing the assertObjectsEqual function two objects and will 
+// assert if two objects are equal and logs the output to the console
 assertObjectsEqual(cd, dc);
 assertObjectsEqual(cd, cd2);
 assertObjectsEqual(dc, cd2);
