0bd35ba37a23a8f8ab1d8bfa079c885ef0540240
DionKanhai <dionkanhais@gmail.com>
2022-09-03 22:44:16 +0000

    refactored code and added comments for better readability

--- takeUntil.js
+++ takeUntil.js
@@ -1,36 +1,13 @@
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// Imports files needed to test the function
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(array1, array2) {
-  if (array1 === array2) {
-    console.log(`The Arrays match!`);
-  } else if (array1 !== array2) {
-    console.log(`The Arrays do not match!`);
-  }
-};
-
-
-// the callback is the filtering if condition
-
-// will need to output the numbers/value of the input array
-// up until the value/number breaks the callback function condition 
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-
+// This function take Until will be passed an array and a callback function
+// The function will use the callback condition to return values into a new array that pass the callback function condition
 const takeUntil = function(array, callback) {
   const results = [];
   for (let item of array) {
   // calls the callback function and checks if the item in the input array 
-  // does not pass the callback if condition, if it does not
+  // does not pass the callback if condition, if it does
   // then push item to results array
   if (callback(item)) {
     return results;
@@ -40,8 +17,8 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
-const results1 = takeUntil(data1, value => value < 0)
+// Test code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, value => value < 0);
 console.log(results1);
-
-//test code
-console.log(assertArraysEqual(takeUntil(data1, value => value < 0)))
+console.log(assertArraysEqual(data1, results1));
