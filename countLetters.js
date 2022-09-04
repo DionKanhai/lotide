c276c954b6267f55d22133c00c06b2bd3dd75bb3
DionKanhai <dionkanhais@gmail.com>
2022-09-04 00:40:16 +0000

    added comments

--- countLetters.js
+++ countLetters.js
@@ -1,31 +1,26 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed`);
-  }
-};
+// imports the function from the sourced file 
+const assertEqual = require('./assertEqual');
 
-// stringsToCount is any amount of strings  
-// countLetters will count the frequency of each letter in the stringsToCount
+/* This function countLetters will take an input string and then return an object
+that has the count of the frequency of each letter in the string
+ */
 const countLetters = function (stringsToCount) {
   const results = {};
-  for (const character of stringsToCount.toLowerCase()) {
+  // make the characters in the string lowercase letters and loop through it
+  for (const character of stringsToCount.toLowerCase()) { 
     if (character !== ' ') {
+      // if the character exists in results object increment value by 1
       if (results[character]) {
-        results[character] += 1; // if key exists in results object increment value by 1
+        results[character] += 1; 
+      // if character does not exist in results object then create the key in the results object with a value of 1
       } else {
-        results[character] = 1;  // if key does not exist in results object then create the key in the results object with a value of 1
+        results[character] = 1; 
       }
     }
   }
   return results;
-}
+};
 
 // test code 
-console.log(countLetters("Lighthouse labs"))
-
-// step 1. break the string into individual letters
-// step 2. loop through the string input of stringsToCount
-// step 3. each string has an index value 
-// step 4. if two index values match add them 
+console.log(countLetters("Lighthouse labs"));
+assertEqual(countLetters("Lighthouse labs"));
