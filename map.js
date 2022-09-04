8f71f313766711a034f5d7c099486c6069458020
DionKanhai <dionkanhais@gmail.com>
2022-09-04 00:43:05 +0000

    added comments and refactored file for better readability

--- map.js
+++ map.js
@@ -1,39 +1,27 @@
-// checks if two arrays are equal or not
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-// asserts if two arrays are equal or not
-const assertArraysEqual = function (array1, array2) {
-  if (array1 === array2) {
-    console.log(`The Arrays match!`);
-  } else if (array1 !== array2) {
-    console.log(`The Arrays do not match!`);
-  }
-};
+// Import files that will test the function
+const assertArraysEqual = require('./assertArraysEqual');
 
-// map function
-const words = ["ground", "control", "to", "major", "tom"];
+/*This map function accepts an array and a callback function as a parameter. It 
+will appy the callback function to each element of the array, and then return a 
+new array based on the condition of the callback function
+*/
 
 const maps = function(array, callback) {
   const results = [];
+  // loop through the input array
   for (let item of array) {
-    results.push(callback(item));
+    //push the items in the array to the new array based on the callback condition
+    results.push(callback(item)); 
   }
   return results;
 };
 
-
-
+//Test code
+const words = ["ground", "control", "to", "major", "tom"];
 const results1 = maps(words, word => word[0]);
 console.log(results1)
+const results2 = ["g", "c", "t", "m", "t"]
 
 // test code
 assertArraysEqual(maps(words, word => word[0])); // prints out the arrays do not match
+assertArraysEqual(maps(results1, results2));
