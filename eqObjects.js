66eda43d752a777b8819636b94586ef442ca8732
DionKanhai <dionkanhais@gmail.com>
2022-09-04 00:40:54 +0000

    added comments

--- eqObjects.js
+++ eqObjects.js
@@ -1,32 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed`);
-  }
-};
+// Imports the functions needed to help run eqObjects
+const eqArrays = require('./eqArrays');
 
-function eqArrays(array1, array2) {
-  console.log(array2)
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+// This funnction is passed two objects, and will return true if both objects have identical keys with identical values,
+// otherwise it returns false
 const eqObjects = function(object1, object2) {
   let objectOneKeys = Object.keys(object1)
   let objectTwoKeys = Object.keys(object2)
   if (objectOneKeys.length !== objectTwoKeys.length) {
     return false;
-  }
+  } 
+  // loops through keys of object, compares the arrays in the keys using
+  // eqArrays, passing the objects to eqArrays as arguments and if they are not equal log fail
   for (const keys in object1) {
     if (Array.isArray(object1[keys]) && Array.isArray(object2[keys])) {
       const resultsArrayComparison = eqArrays(object1[keys], object2[keys])
@@ -34,6 +18,7 @@ const eqObjects = function(object1, object2) {
         return false;
       }
     } 
+     // if object keys do not match then return false
     else if (object1[keys] !== object2[keys]) {
      return false;
     } 
@@ -41,7 +26,7 @@ const eqObjects = function(object1, object2) {
   return true
 };
 
-// test case that should return true
+// Test cases for the eqObject function
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(eqObjects(ab, ba)); 
