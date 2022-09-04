fafd6b8dce8d172d6405ff0c82656c82b608aa24
DionKanhai <dionkanhais@gmail.com>
2022-09-04 00:41:47 +0000

    added comments for better readability

--- flatten.js
+++ flatten.js
@@ -1,25 +1,28 @@
-function eqArrays(array1, array2) {
-  if (array1.join() === array2.join()) {
-    return true;
-  } else if (array1.join() !== array2.join()) {
-    return false;
-  }
-}
+// Import the functions from the sourced file
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(array1, array2) {
-  if (array1 === array2) {
-    console.log(`The Arrays match!`);
-  } else if (array1 !== array2) {
-    console.log(`The Arrays do not match!`);
-  }
-};
-
+/* This function can be passed an array that can be nested or not, 
+and return an array that it not nested */
 function flatten(arrays) {
+  //check if the user passed an array
   if (Array.isArray(arrays)) { 
-    for (let i = 0; i < arrays.length; i++) { 
-      return arrays.flat() 
+    for (let i = 0; i < arrays.length; i++) {
+      // remove nested arrays using the flat method 
+      return arrays.flat(); 
      }
     }
   };
-console.log(flatten([1, 2, [3, 4], 5, [6]]))
-   
+
+// Test code
+const nestedArray = [1, 2, [3, 4], 5, [6]];
+assertArraysEqual(flatten(nestedArray, [1, 2, [3, 4], 5, [6]])); // => arrays do not match
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([[10, 4], [34, 6], [92, 2]]));
+console.log(flatten([1, 3, 3]));
+
+let afterFlat = flatten(nestedArray);
+console.log(afterFlat);
+console.log(eqArrays([1, 2, 3, 4, 5, 6], afterFlat)); // => logs true
+console.log(eqArrays([1, 2, [3, 4], 5, [6]], afterFlat)); // => logs false
