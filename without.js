54a75db7d696ce378b8a206c38be367828cd8f31
DionKanhai <dionkanhais@gmail.com>
2022-09-07 00:33:05 +0000

    modified the function

--- without.js
+++ without.js
@@ -5,17 +5,10 @@ const assertArraysEqual = require('./assertArraysEqual');
 // The second array will determine aa value to remove from the first array
 // The function will remove that value and return a new array without the value
 function without(source, itemsToRemove) {
-  // loop through the source array
-  for (let i = 0; i < itemsToRemove.length; i++) {
-    // store the value to be removed in a variable
-    const remove = source.includes(itemsToRemove[i])
-    // if vale is found in array splice it and return new array
-    if (remove) {
-      source.splice(i, 1)
-    }
-  }
-  return source;
-};
+  // take in the source array and use the filter method on each element in the array
+  // if the element is not item to be removed return element
+  return source.filter(element => !itemsToRemove.includes(element));
+} 
 
 // Test code
 console.log(without([1, 2, 3], [1]));
@@ -23,3 +16,4 @@ console.log(without([4, 5], [4]));
 
 assertArraysEqual(without([1, 2, 3], [1]));
 
+module.exports = without;
