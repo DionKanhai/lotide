e5c371028e3db9ff9b0e2c8713a62ec1419bf114
DionKanhai <dionkanhais@gmail.com>
2022-09-07 00:55:22 +0000

    modified the function to import another function to help run the test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,17 @@
+const eqArrays = require('./eqArrays');
+
 // Function that is passed in an two arrays and outputs an assertion message if the two arrays match or not
 const assertArraysEqual = function(array1, array2) {
   // check if arrays match and if they do logs the message
-  if (array1 === array2) {
-    console.log(`The Arrays match!`);
+    if (!eqArrays(array1, array2)) {
+    console.log('The Arrays do not match!');
+    
     // checks if two do not match and logs the message
-  } else if (array1 !== array2) {
-    console.log(`The Arrays do not match!`);
+}
+  else {
+    console.log('The Arrays match!')
   }
-};
+  }
 
 // Enable this function to be exported to other files
 module.exports = assertArraysEqual;
