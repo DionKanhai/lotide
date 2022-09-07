d249a38906da03203582f3e6413315e133f69d32
DionKanhai <dionkanhais@gmail.com>
2022-09-07 00:53:42 +0000

    modified the test

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,9 +1,8 @@
 //Imports the functions from the source file for test use
 const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
 
 // Test code 
-assertArraysEqual(eqArrays([2, 3, 4], [2, 3, 4]), true); //  logs match
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); // logs do not match
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // logs match
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // logs do not match
+assertArraysEqual([2, 3, 4], [2, 3, 4]); //  logs match
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // logs does not match
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // logs does not match
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // logs do match
